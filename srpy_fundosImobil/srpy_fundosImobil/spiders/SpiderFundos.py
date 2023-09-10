1cc98e5b215985970b6c729038b11eb7308004b1
Matheus Oliveira <114704235+greenShimada@users.noreply.github.com>
2023-09-10 19:34:55 -0300

    Saída de testes para arquivo TXT, não formatado.

--- srpy_fundosImobil/srpy_fundosImobil/spiders/SpiderFundos.py
+++ srpy_fundosImobil/srpy_fundosImobil/spiders/SpiderFundos.py
@@ -6,10 +6,10 @@ class FundosSpider(scrapy.Spider):
     start_urls = ['https://investidor10.com.br/fiis/']
 
     def parse(self, response):
-        links = response.xpath('//section[@class="section-sectors"]//div[@class="actions fii"]//a/@href')
+        links = response.xpath('//section[@class="section-sectors"]//div[@class="actions fii"]//a/@href').getall()
         for link in links:
             yield scrapy.Request(
-                response.urljoin(link),
+                link,
                 callback=self.parse_each
             )
 
@@ -18,6 +18,9 @@ class FundosSpider(scrapy.Spider):
         cot_dy_val =  response.xpath('//div[@class="_card-body"]/div/span/text()').getall()
         pvp_liq =  response.xpath('//div[@class="_card-body"]/span/text()').getall()
         tipo_de_fundo = response.xpath('//div[@class="cell"]/div[@class="desc"]/span[text()="\nTIPO DE FUNDO\n"]/following::div/span/text()').get()
-
+        arq = open("arq.txt", "a")
+        arq.writelines(cot_dy_val)
+        arq.writelines (pvp_liq)
+        arq.writelines (tipo_de_fundo)
         pass
 
