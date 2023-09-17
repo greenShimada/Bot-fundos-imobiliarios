f76b8054cffe99b393bc997c3c154a71e9b057c9
Matheus Oliveira <114704235+greenShimada@users.noreply.github.com>
2023-09-17 19:29:31 -0300

    Percorre todas as paginas e pega mais informações

--- srpy_fundosImobil/srpy_fundosImobil/spiders/SpiderFundos.py
+++ srpy_fundosImobil/srpy_fundosImobil/spiders/SpiderFundos.py
@@ -1,26 +1,52 @@
 import scrapy
 import requests
 
+
 class FundosSpider(scrapy.Spider):
+
     name = 'example'
     start_urls = ['https://investidor10.com.br/fiis/']
 
     def parse(self, response):
-        links = response.xpath('//section[@class="section-sectors"]//div[@class="actions fii"]//a/@href').getall()
-        for link in links:
+        paginas = response.xpath('//section[@class="section-sectors"]//div[@class="section-sectors-pagination"]//li[@class="pagination-item"]//a/@href').getall()
+        for pagina in paginas:
             yield scrapy.Request(
-                link,
+                pagina,
                 callback=self.parse_each
             )
 
-
     def parse_each(self, response):
-        cot_dy_val =  response.xpath('//div[@class="_card-body"]/div/span/text()').getall()
-        pvp_liq =  response.xpath('//div[@class="_card-body"]/span/text()').getall()
+        links = response.xpath('//section[@class="section-sectors"]//div[@class="actions fii"]//a/@href').getall()
+
+        for link in links:
+            yield scrapy.Request(
+                link,
+                callback=self.parse_each_fundo
+            )
+
+    def parse_each_fundo(self, response):
+
+        from unidecode import unidecode
+
+        nome = response.css("h1::text").get()
+        cotacao =  response.xpath('//div[@class="_card-body"]/div/span/text()').get()
+        pvp =  response.xpath('//div[@class="_card-body"]/span/text()').get()
+        div = response.xpath('//div[@class="_card dy"]//div[@class="_card-body"]/div/span/text()').get()
         tipo_de_fundo = response.xpath('//div[@class="cell"]/div[@class="desc"]/span[text()="\nTIPO DE FUNDO\n"]/following::div/span/text()').get()
-        arq = open("arq.txt", "a")
-        arq.writelines(cot_dy_val)
-        arq.writelines (pvp_liq)
-        arq.writelines (tipo_de_fundo)
+        todos_imoveis = response.xpath('//section[@class="section-sectors"]//div[@class="container"]//div[@id="properties-section"]//div[@id="container-properties"]//div//h3')
+        vacancia = response.xpath('//div[@class="cell"]/div[@class="desc"]/span[text()="\nVACÂNCIA\n"]/following::div/span/text()').get()
+        segmento = response.xpath('//div[@class="cell"]/div[@class="desc"]/span[text()="\nSEGMENTO\n"]/following::div/span/text()').get()
+
+        yield {
+            'nome': nome,
+            'cotacao: ': cotacao,
+            'dividend yield': div,
+            'pvp_liquidez': pvp,
+            'tipo_de_fundo': unidecode(tipo_de_fundo),
+            'qntd imoveis': len(todos_imoveis),
+            'vacancia': vacancia,
+            'segmento': unidecode(segmento)
+        }
+
         pass
 
